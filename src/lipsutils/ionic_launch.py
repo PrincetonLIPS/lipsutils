7055688427d5e65c69da2c45aa0e023f5217affa
Nick Richardson <njkrichardson@princeton.edu>
2024-12-14 10:41:55 -0500

    bug: off-by-one validation error disallowed requesting 8 gpus

--- src/lipsutils/ionic_launch.py
+++ src/lipsutils/ionic_launch.py
@@ -1,39 +1,46 @@
 import dataclasses
-import os 
+import os
 from pathlib import Path
 import subprocess
 from typing import Optional
 
-import tyro 
+import tyro
+
 
 @dataclasses.dataclass
-class CLIArgs: 
-    node: str 
-    cpus: Optional[int] = 1 
-    memory: Optional[str]="32G"
-    gpus: Optional[int] = 0 
-    account: Optional[str] = "lips" 
-    reservation: Optional[str] = "lips-interactive" 
+class CLIArgs:
+    node: str
+    cpus: Optional[int] = 1
+    memory: Optional[str] = "32G"
+    gpus: Optional[int] = 0
+    account: Optional[str] = "lips"
+    reservation: Optional[str] = "lips-interactive"
 
-def validate_args(config: CLIArgs): 
-    if (config.cpus >= 64) or (config.cpus < 1): 
-        raise ValueError(f"Must request a number of cpus between 1 and 64 inclusive, but asked for {config.cpus}")
-    if (config.gpus >= 8) or (config.gpus < 0): 
-        raise ValueError(f"Must request a number of gpus between 0 and 8 inclusive, but asked for {config.gpus}")
 
-def main(config: CLIArgs): 
-    """Basic driver for a call to salloc to request an interactive reservation on our lips-interactive daily 
-    flex reservation. 
+def validate_args(config: CLIArgs):
+    if (config.cpus >= 64) or (config.cpus < 1):
+        raise ValueError(
+            f"Must request a number of cpus between 1 and 64 inclusive, but asked for {config.cpus}"
+        )
+    if (config.gpus > 8) or (config.gpus < 0):
+        raise ValueError(
+            f"Must request a number of gpus between 0 and 8 inclusive, but asked for {config.gpus}"
+        )
+
+
+def main(config: CLIArgs):
+    """Basic driver for a call to salloc to request an interactive reservation on our lips-interactive daily
+    flex reservation.
 
     Parameters
     ----------
-    config.node : str 
+    config.node : str
         The node identifier (see `lipsutils.ionic_info.main`)
-    config.cpus : int 
-        Number of cpus to request for the reservation (default, 1). 
-    config.memory : str 
+    config.cpus : int
+        Number of cpus to request for the reservation (default, 1).
+    config.memory : str
         String memory request (default: "32G")
-    config.gpus : int 
+    config.gpus : int
         Non-negative number of gpus to request for the reservation (default, 0).
 
     Example
@@ -45,22 +52,23 @@ def main(config: CLIArgs):
     """
     validate_args(config)
     args = [
-        "salloc", 
-        f"--gres=gpu:{config.gpus}", 
-        "-c", 
-        f"{config.cpus}", 
+        "salloc",
+        f"--gres=gpu:{config.gpus}",
+        "-c",
+        f"{config.cpus}",
         "-A",
-        f"{config.account}", 
-        f"--reservation={config.reservation}", 
-        f"--nodelist={config.node}", 
-        f"--mem={config.memory}", 
-        "srun", 
-        "--pty", 
-        f"{os.environ['SHELL']}", 
-        "-l"
+        f"{config.account}",
+        f"--reservation={config.reservation}",
+        f"--nodelist={config.node}",
+        f"--mem={config.memory}",
+        "srun",
+        "--pty",
+        f"{os.environ['SHELL']}",
+        "-l",
     ]
     subprocess.run(args)
 
-if __name__=="__main__": 
+
+if __name__ == "__main__":
     config = tyro.cli(CLIArgs)
     main(config)
